5547bb7bb9ac20c99723258c05cf889c5d40cd5e
enmanuel <enmanuel@cocoa>
2017-09-05 12:59:00 -0400

    added characteristics such as speed, color, and shape to the turtle

--- class_turtle.py
+++ class_turtle.py
@@ -4,9 +4,19 @@
 import turtle
 
 def draw_square():
-	window = turtle.Screen()
-	window.bgcolor("red")
+	window = turtle.Screen() #sets the screen for the turtle
+	window.bgcolor("red") #sets the color of the screen
+	
+	#creates a turtle object name brad.
 	brad = turtle.Turtle()
+
+	#changes the color of the turtle
+	brad.color("blue")
+	#changes the shape of the turtle
+	brad.shape("turtle")
+	#changes the speed of the turtle
+	brad.speed(5)
+	#creates a square
 	brad.forward(100)
 	brad.right(90)
 	brad.forward(100)
@@ -15,6 +25,8 @@ def draw_square():
 	brad.right(90)
 	brad.forward(100)
 	brad.right(90)
+
+	#class a function that allow the progem to close upon click.
 	window.exitonclick()
 
 draw_square()
