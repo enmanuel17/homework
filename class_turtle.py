a63219c705cec6f7ff8431b76ca8923e0624ae2b
enmanuel <enmanuel@cocoa>
2017-09-05 13:55:55 -0400

    added another object that creates a circle of squares

--- class_turtle.py
+++ class_turtle.py
@@ -43,11 +43,30 @@ def draw_triangle():
 		victor.forward(100) # go forward 100
 		victor.left(120) #move to the left 120 degrees.
 		sides = sides + 1 #counter to exit the while loop.
-		
+
+#Function that creates a circle out of squares.
+def draw_star():
+	star = turtle.Turtle() #creates the object
+	star.speed(0) # sets the speed of the drawing.
+	circle = 360 #hold the amount of degress a circle has
+	degressDraw = 0 #counter variable for keeping track of degrees.
+
+	#this while loop creates squares until it reacheas 360 degrees of squares.
+	while (degressDraw < 360):
+		#creates a circle with a while loop
+		sides = 0
+		while (sides < 4):
+			star.forward(50)
+			star.right(90)
+			sides = sides + 1 #counter to make the while loop end.
+		degressDraw = degressDraw + 1 #increase the counter
+		star.left(degressDraw) #moves the turtle pointer to the next degree to go on  a circle.
+
 #Calls each function to create each object.
 draw_square()
 draw_circle()
 draw_triangle()
+draw_star()
 
 #class a function that allow the progem to close upon click.
 window.exitonclick()
