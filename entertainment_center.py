fa912c08acbe23fb2db71af14499905b00d29a41
Enmanuel Hernandez <enmanuelh17@gmail.com>
2017-09-05 20:27:18 -0400

    added another movie object and a method to open trailers for the class

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,7 +4,18 @@
 
 import media
 
+#creates a toy story movie object
 toy_story = media.Movie("Toy Story","Story of a boy and his toys that come to life","http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","http://www.youtube.com/watch?v=vwyZH85NQC4")
 
+
+#creates an avatar movie object
+avatar = media.Movie("Avatar","A marine on an alien planet","http://upload.wikimedia.org/wikimedia/id/b/b0/Avatar-Teaser-Post","http://www.youtube.com/watch?v=-9ceBgWV8io")
+
+#create the 300 spatants movei object(my favorite movei)
+the300spartants = media.Movie("300","King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 B.C.","https://en.wikipedia.org/wiki/File:300poster.jpg","https://www.youtube.com/watch?v=ZJ6yq-oVKPc")
+
 print(toy_story.storyline)
+#avatar.show_trailer()
+print(the300spartants.storyline)
+the300spartants.show_trailer()
 
--- media.py
+++ media.py
@@ -2,6 +2,7 @@
 #movie trailer website
 #part of code are from udacity learning material
 
+import webbrowser
 #class that make the blueprint for movie objects
 class Movie():
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -9,4 +10,8 @@ class Movie():
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
-
+	
+	#opens a web browser with the trailer of the movie
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
+	
